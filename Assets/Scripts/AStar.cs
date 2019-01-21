e9bd7e9a379ee2560abb570f7bac3d87917319f0
Ricky Avina <rickyavina@icloud.com>
2019-01-20 19:47:41 -0500

    Initial commit, grid is set up

---
+++ Assets/Scripts/AStar.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AStar : MonoBehaviour
+{
+    [Tooltip("Resolution of the grid, ex: 10 will produce 10x10 grid")]
+    public int n;
+
+    [Tooltip("Coordinates of the startingNode")]
+    public int startingX, startingY;
+    [Tooltip("Coordinates of the goalNode")]
+    public int goalX, goalY;
+
+    private Node startNode;
+    private Node goalNode;
+
+    List<List<Node>> grid;  // Reference to the entire n x n grid of nodes
+
+    List<Node> openNodes;   // List of nodes for which F-Cost have been calculated
+    List<Node> closedNodes; // List of nodes that have already been evaluated
+
+    private Node currentNode;   // used for tracking in loop
+
+    void Start()
+    {
+        initializeNodes();  // Init nodes with default values
+        //findPath();
+    }
+
+    public void findPath()
+    {
+        while (true) // not reached goal or explored every node
+        {
+            currentNode = openNodes[findNodeWithLowestFCost()]; // Node with the highest fVal
+            openNodes.Remove(currentNode);
+            closedNodes.Add(currentNode);   // Node has already been explored
+
+            if (currentNode == goalNode)    // path has been found
+            {
+                return;
+            }
+
+            for (int i = 0; i < currentNode.neighbors.Count; i++)
+            {
+                if (!currentNode.neighbors[i].isObstacle && !inClosedSet(currentNode.neighbors[i]))   // make sure neighbors aren't obstacle and haven't been explored yet
+                {
+                    // calculate g,h,f values for nodes
+                    currentNode.neighbors[i].gCost = currentNode.gCost + costToTravelToNode(currentNode.neighbors[i], currentNode);
+                    // set parents of nodes 
+                    // check if the movement cost from neighbor node back to origin is smaller
+                    //if (new path to neighbor is shorter OR !inOpenSet(currentNode.neighbors[i])) {
+
+                    //}
+                }
+            }
+        }
+    }
+
+
+    //private int pathToNeighbor(Node currentNode, Node neighborNode, int totalCost = 0)
+    //{
+    //    // go down gCost of traversing through neighbors
+    //    Node parent = currentNode.parent;
+
+    //    if (parent == null) {
+    //        return totalCost;
+    //    }
+    //    else {
+    //        totalCost += costToTravelToNode(currentNode, parentNode);
+
+    //        return 1;
+    //    }
+    //}
+
+    private int costToTravelToNode(Node currentNode, Node parentNode)   // currentNode can be though as the neighbor
+    {
+ 
+        return Convert.ToInt32(10*Math.Sqrt(Math.Pow(currentNode.y - parentNode.y, 2) + Math.Pow(currentNode.x - parentNode.x, 2)));
+    }
+
+    private bool inClosedSet(Node node)
+    {
+        return closedNodes.Contains(node);
+    }
+
+    private bool inOpenSet(Node node)
+    {
+        return openNodes.Contains(node);
+    }
+
+    private int findNodeWithLowestFCost()
+    {
+        int lowest = openNodes[0].fCost;
+        int index = 0;
+        for (int i = 0; i < openNodes.Count; i++)
+        {
+            if (openNodes[i].fCost < lowest)
+            {
+                lowest = openNodes[i].fCost;
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    void initializeNodes()
+    {
+        openNodes = new List<Node>();
+        closedNodes = new List<Node>();
+
+        int count = 0;
+        grid = new List<List<Node>>(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            List<Node> temp = new List<Node>(n);
+            for (int j = 0; j < n; j++)
+            {
+                Node node = new Node(count, i, j);    // add Node with nodeNumber
+                temp.Add(node);
+                count++;
+            }
+            grid.Add(temp);
+        }
+
+        // initialize the starting Node and goal Nodes
+        startNode = grid[startingX][startingY];
+        startNode.gCost = 0;        // starting Node has a gCost of 0
+        startNode.fCost = 0;
+        openNodes.Add(startNode);  // add startingNOdes to openNodes for use in the first iteration of findPath
+
+        goalNode = grid[goalX][goalY];          // keep reference to the goal node
+        initializeNodeNeighbors();
+    }
+
+    void initializeNodeNeighbors()
+    {
+        // initialize neighbors for all Nodes
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++) // assuming n x n grid
+            {
+                    if (i > 0 && j > 0) // make sure top left can be added
+                    {
+                        grid[i][j].neighbors.Add(grid[i - 1][j - 1]);
+                    }
+                    if (i < n-1 && j > 0) {     // top right
+                        grid[i][j].neighbors.Add(grid[i + 1][j - 1]);
+                    }
+                    if (i > 0) // make sure left can be added
+                    {
+                        grid[i][j].neighbors.Add(grid[i - 1][j]);
+                    }
+                    if (j > 0) // make sure top can be added
+                    {
+                        grid[i][j].neighbors.Add(grid[i][j - 1]);
+                    }
+                    if (j < n-1)  // make sure bottom can be added
+                    {
+                        grid[i][j].neighbors.Add(grid[i][j + 1]);
+                    }
+                    if (i < n-1) // make sure right can be added
+                    {
+                        grid[i][j].neighbors.Add(grid[i + 1][j]);
+                    }
+                    if (i < n-1 && j < n-1) // make sure bottom right can be added
+                    {
+                        grid[i][j].neighbors.Add(grid[i + 1][j + 1]);
+                    }
+                    if (i > 0 && j < n - 1) // bottom left
+                    {
+                        grid[i][j].neighbors.Add(grid[i - 1][j + 1]);
+                    }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+internal class Node
+{
+    int nodeNumber;
+    public int x;  // x pos in grid
+    public int y;  // y pos in grid
+    public int gCost;
+    int hCost;
+    public bool isObstacle = false;
+    public int fCost;
+    public Node parent;
+    public List<Node> neighbors;   // Each neighbor has a GCOST (cost of moving there) and HCost (how far that node is from start) anf Fcost (G+H)
+                            // These values are being recalculated every step
+                            // if 2 nodes have the same fcost, use lowest HCost
+                            // All of the neighbors are parented to the original node they came from, only parent if other noddes dont have parent or if new parent has better f ost
+
+    public Node(int nodeNumber, int x, int y)
+    {
+        this.nodeNumber = nodeNumber;
+        this.x = x;
+        this.y = y;
+        neighbors = new List<Node>();   // neighbors will be of varied list for every Node
+        //this.hValue = hValue;
+    }
+
+    public static bool operator ==(Node n1, Node n2)
+    {
+        return n1.nodeNumber == n2.nodeNumber;
+    }
+
+    public static bool operator !=(Node n1, Node n2)
+    {
+        return n1.nodeNumber != n2.nodeNumber;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return $"Node {nodeNumber}";
+    }
+}
